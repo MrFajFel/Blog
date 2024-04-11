aa0f4e130b88d81d55e867ce989d915d6940fb03
skiparb <skiparb@gmail.com>
2024-04-11 14:59:33 +0200

    naprawa25

--- Aplikacja/views.py
+++ Aplikacja/views.py
@@ -1,9 +1,8 @@
-
-
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from Aplikacja.forms import CommentForm
 from Aplikacja.models import Post
 
 """
@@ -27,7 +26,17 @@ def post_detail(request, year, month, day, slug):
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+
+    comment_form = CommentForm()
     return render(request, "blog/post/detail.html",
-                  {'post': post})
+                  {'post': post, 'comments': comments, 'comment_form': comment_form})
 
 # Create your views here.
